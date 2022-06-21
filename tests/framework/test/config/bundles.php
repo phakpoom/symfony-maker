f63bc6c062eda55c3764c01f8b92dd6eb47cabcc
phakpoom <12756630+phakpoom@users.noreply.github.com>
2022-06-21 19:09:55 +0700

    8

--- tests/framework/test/config/bundles.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Bonn\Maker\Bridge\MakerBundle\BonnMakerBundle::class => ['dev' => true, 'test' => true],
-];
